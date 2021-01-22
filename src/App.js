af51af165bca0961eb2ebc58508a6380198918ce
Greg Johnson <grj923@gmail.com>
2021-01-22 14:13:43 -0500

    Assessment Complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,36 @@ import "./App.css";
 import ScoreBoard from "./components/Scoreboard/Scoreboard";
 import Game from "./components/Game/Game";
 import Team from "./components/Team/Team";
-function App() {
+function App(props) {
+  const raccoons = {
+    name: "Russiaville Raccoons",
+    logoSrc: "./assets/images/raccoon.png",
+  };
+
+  const squirrels = {
+    name: "Sheridan Squirrels",
+    logoSrc: "./assets/images/squirrel.png",
+  };
+
+  const bunnies = {
+    name: "Burlington Bunnies",
+    logoSrc: "./assets/images/bunny.png",
+  };
+
+  const hounds = {
+    name: "Hammond Hounds",
+    logoSrc: "./assets/images/hound.png",
+  };
+
   return (
     <div className="App">
-      <Game />
+      <Game
+        venue="Union 525 Gem"
+        homeTeam={squirrels}
+        visitingTeam={raccoons}
+      />
+      <Game venue="Sheridan Arena" homeTeam={bunnies} visitingTeam={hounds} />
     </div>
   );
 }
-
 export default App;
--- src/components/Scoreboard/Scoreboard.js
+++ src/components/Scoreboard/Scoreboard.js
@@ -1,8 +1,18 @@
 import React from "react";
 function ScoreBoard(props) {
   return (
-    <div classname="ScoreBoard">
-      <h1>Scoreboard Goes Here</h1>
+    <div className="ScoreBoard">
+      <div className="teamStats">
+        <h3>VISITORS</h3>
+        <h3>{props.visitingTeamStats.score}</h3>
+      </div>
+
+      <h3>SCOREBOARD</h3>
+
+      <div className="teamStats">
+        <h3>HOME</h3>
+        <h3>{props.homeTeamStats.score}</h3>
+      </div>
     </div>
   );
 }
